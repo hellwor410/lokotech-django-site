a2b98351872569c6c2b04794e1901ee786d46158
hellwor410 <anatoliigytar2222@gmail.com>
2026-05-12 00:02:35 +1100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -56,8 +56,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy',
     }
 }
 
