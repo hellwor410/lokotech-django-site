35be324df77afc4f5402f7d799403ae7628f5823
hellwor410 <anatoliigytar2222@gmail.com>
2026-05-12 21:04:45 +1100

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Главная страница из main")
+    return HttpResponse("Главная страница ЛокоТех-Сервис")
 
 def about(request):
-    return HttpResponse("Страница о компании")
+    return HttpResponse("О компании: ЛокоТех-Сервис — ремонт локомотивов")
 
 def contacts(request):
-    return HttpResponse("Контакты")
+    return HttpResponse("Контакты: 8-800-555-ЛОКО | service@lokotech.ru")
