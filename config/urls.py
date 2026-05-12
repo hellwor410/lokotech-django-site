46d9d3aede76c6bbe18308b58e227afe615f87ef
hellwor410 <anatoliigytar2222@gmail.com>
2026-05-12 20:45:04 +1100

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,9 @@
-python
 from django.http import HttpResponse
 from django.urls import path
 
 def home(request):
-    return HttpResponse("✅ Сайт ЛокоТех-Сервис работает!")
+    return HttpResponse("✅ Vercel + Django 5.1.2 работает!")
 
 urlpatterns = [
     path('', home),
-    path('admin/', admin.site.urls),
 ]
