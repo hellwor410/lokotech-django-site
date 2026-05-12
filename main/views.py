424db8befcb0b9c850f38f980cde260059b4f22c
hellwor410 <anatoliigytar2222@gmail.com>
2026-05-12 20:57:29 +1100

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render, redirect
-from .forms import RepairRequestForm
-from .models import RepairRequest
+from django.http import HttpResponse
 
 def home(request):
-    if request.method == 'POST':
-        form = RepairRequestForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = RepairRequestForm()
-    return render(request, 'main/home.html', {'form': form})
+    return HttpResponse("Главная страница из main")
+
+def about(request):
+    return HttpResponse("Страница о компании")
+
+def contacts(request):
+    return HttpResponse("Контакты")
