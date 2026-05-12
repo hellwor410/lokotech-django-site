aaedccd7ee7594a506a0b6e0088b6479bc6c2995
hellwor410 <anatoliigytar2222@gmail.com>
2026-05-12 20:59:14 +1100

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 
-def home(request):
+def temp_home(request):
     return HttpResponse("✅ Vercel + Django 5.1.2 работает!")
 
 urlpatterns = [
-    path('', home),
+    path('', temp_home),  # временная заглушка
     path('admin/', admin.site.urls),
+    path('main/', include('main.urls')),  # временно на /main/
 ]
