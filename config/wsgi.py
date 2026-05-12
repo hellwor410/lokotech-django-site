0c0559550b8a93135debfcdc4696b6e257d40687
hellwor410 <anatoliigytar2222@gmail.com>
2026-05-12 19:53:41 +1100

    Update wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,10 +1,5 @@
 import os
-import sys
 
-# Добавляем путь к проекту (на случай, если Vercel его не видит)
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-# Явно указываем, где искать настройки
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 from django.core.wsgi import get_wsgi_application
