4e23f9c6683639f5053db8d1db7a501abe622d3e
hellwor410 <anatoliigytar2222@gmail.com>
2026-05-12 21:28:58 +1100

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contacts/', views.contacts, name='contacts'),
+    path('create-request/', views.create_request, name='create_request'),
 ]
