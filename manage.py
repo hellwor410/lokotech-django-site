0b3b8ad5f85315d6b518cb084a23b817d965a5ef
hellwor410 <anatoliigytar2222@gmail.com>
2026-05-12 19:54:18 +1100

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-# Это самая важная строка для Vercel!
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
 def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +15,4 @@ def main():
 if __name__ == '__main__':
     main()
 
-# Vercel нужна переменная 'app' для запуска
 from config.wsgi import app
