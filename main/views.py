576dae4ec05b1e51b35bb6edf0eb2e1c9bdd035b
hellwor410 <anatoliigytar2222@gmail.com>
2026-05-12 21:10:39 +1100

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,10 +1,22 @@
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import RepairRequest
 
 def home(request):
-    return HttpResponse("Главная страница ЛокоТех-Сервис")
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        problem = request.POST.get('problem')
+        RepairRequest.objects.create(
+            name=name,
+            phone=phone,
+            problem=problem
+        )
+        return redirect('home')
+    return render(request, 'main/home.html')
 
 def about(request):
-    return HttpResponse("О компании: ЛокоТех-Сервис — ремонт локомотивов")
+    return render(request, 'main/about.html')
 
 def contacts(request):
-    return HttpResponse("Контакты: 8-800-555-ЛОКО | service@lokotech.ru")
+    return render(request, 'main/contacts.html')
